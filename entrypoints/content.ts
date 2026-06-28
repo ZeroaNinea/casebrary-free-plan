ba0b9363c99b67a331640e2d094c0a2d0a73a312
Catgirl <zeroaninea@gmail.com>
2026-06-28 23:05:07 +0400

    use .clear

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -23,5 +23,21 @@ export default defineContentScript({
     console.log(await db.getAll('entries'));
 
     await db.delete('entries', '1');
+
+    await db.put('entries', {
+      id: '2',
+      title: 'Hello 2',
+      parentId: null,
+      type: EntryType.Note,
+      properties: {},
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    } as Entry);
+
+    console.log(await db.getAll('entries'));
+
+    await db.clear('entries');
+
+    console.log(await db.getAll('entries'));
   },
 });
