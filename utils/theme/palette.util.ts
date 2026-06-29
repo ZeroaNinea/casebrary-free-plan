93781efd3ae65c121cb217f77ba12b311c8445ff
Catgirl <zeroaninea@gmail.com>
2026-06-29 20:40:55 +0400

    the old code removed

--- utils/theme/palette.util.ts
+++ utils/theme/palette.util.ts
@@ -25,38 +25,4 @@ export default function createPalette(base: string): Palette {
   };
 
   return palette;
-
-  // return mode === 'light'
-  //   ? ({
-  //       50: scale[0],
-  //       100: scale[1],
-  //       200: scale[2],
-  //       300: scale[3],
-  //       400: scale[4],
-  //       500: scale[5],
-  //       600: scale[6],
-  //       700: scale[7],
-  //       800: scale[8],
-  //       900: scale[9],
-  //       hover: chroma(base).brighten(0.5).hex(),
-  //       active: chroma(base).darken(0.5).hex(),
-  //       focus: chroma(base).saturate(1).hex(),
-  //       base: base,
-  //     } as Palette)
-  //   : ({
-  //       50: scale[9],
-  //       100: scale[8],
-  //       200: scale[7],
-  //       300: scale[6],
-  //       400: scale[5],
-  //       500: scale[4],
-  //       600: scale[3],
-  //       700: scale[2],
-  //       800: scale[1],
-  //       900: scale[0],
-  //       hover: chroma(base).brighten(0.5).hex(),
-  //       active: chroma(base).darken(0.5).hex(),
-  //       focus: chroma(base).saturate(1).hex(),
-  //       base: base,
-  //     } as Palette);
 }
