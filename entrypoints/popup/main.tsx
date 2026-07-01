457eb587eec7ede473caa8215524121d1c82f47b
Catgirl <zeroaninea@gmail.com>
2026-07-01 15:00:46 +0400

    use provider in the main.tsx

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
 import App from './App.tsx';
 
 import '@/utils/i18n/';
 
 import defaultTheme from '@/utils/theme/default-theme';
 import applyTheme from '@/utils/theme/apply-theme';
+
+import store from '@/utils/store';
 
 applyTheme(defaultTheme);
 
@@ -14,6 +18,8 @@ import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
