dc1d9461d6c4c2e581abd5fca393b5db641a38e8
Catgirl <zeroaninea@gmail.com>
2026-07-02 14:28:09 +0400

    unused code removed

--- entrypoints/components/pages/entry-editor.page.tsx
+++ entrypoints/components/pages/entry-editor.page.tsx
@@ -7,7 +7,7 @@ import IconPicker from '@/entrypoints/components/icon-picker';
 
 import CurrentPage from '@/types/current-page.alias';
 
-import { icons, IconName } from '@/utils/icons';
+import { IconName } from '@/utils/icons';
 
 export default function EntryEditorPage({
   show,
@@ -52,65 +52,6 @@ export default function EntryEditorPage({
           //   // TODO: set title
           // }}
         />
-        {/* <div className="flex gap-2 my-3">
-          <TransparentPillButton
-            isState={iconMode === 'lucide'}
-            onClick={() => setIconMode('lucide')}
-          >
-            {t('icons')}
-          </TransparentPillButton>
-
-          <TransparentPillButton
-            isState={iconMode === 'url'}
-            onClick={() => setIconMode('url')}
-          >
-            {t('imageUrl')}
-          </TransparentPillButton>
-
-          <TransparentPillButton
-            isState={iconMode === 'upload'}
-            onClick={() => setIconMode('upload')}
-          >
-            {t('upload')}
-          </TransparentPillButton>
-        </div>
-        {iconMode === 'lucide' && (
-          <div className="flex flex-wrap justify-center gap-2">
-            {Object.entries(icons).map(([name, Icon]) => (
-              <IconButton
-                key={name}
-                isState={icon === name}
-                onClick={() => setIcon(name as IconName)}
-              >
-                <Icon size={18} />
-              </IconButton>
-            ))}
-          </div>
-        )}
-        {iconMode === 'url' && (
-          <TitleField
-            label={t('imageUrl')}
-            placeholder="https://example.com/icon.png"
-            value={iconUrl}
-            onChange={setIconUrl}
-            icon="image"
-          />
-        )}
-        {iconMode === 'upload' && (
-          <div
-            className="
-              rounded-lg
-              border border-border
-              p-4
-              text-center
-              text-text-muted
-            "
-          >
-            <p className="font-medium">{t('imageUploading')}</p>
-
-            <p className="mt-2 text-sm">{t('imageUploadingDescription')}</p>
-          </div>
-        )} */}
         <IconPicker
           icon={icon}
           setIcon={setIcon}
